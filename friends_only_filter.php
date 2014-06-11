eecec6701ca21e9240ba76724219e5d5012ed921
Jeremy Pry <JPry@users.noreply.github.com>
2014-06-11 15:38:59 -0400

    Create friends_only_filter.php

---
+++ friends_only_filter.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Plugin Name: Friends Only Filter
+ * Plugin URI: https://github.com/PrysPlugins/friends-only-filter
+ * Description: Filter the "Friends Only" plugin to allow more exceptions.
+ * Version: 1.0
+ * Author: Jeremy Pry
+ * Author URI: http://jeremypry.com/
+ * License: GPL2
+ */
+
+// Prevent direct access to this file
+if ( ! defined( 'ABSPATH' ) ) {
+	die( "You can't do anything by accessing this file directly." );
+}
+
+add_filter( 'fo_sentry_off', 'jpry_fo_sentry_off' );
+/**
+ * Turn off the Friends Only Sentry if our custom header is present.
+ *
+ * @since 1.0
+ *
+ * @param bool $sentry_off Whether the sentry is currently off.
+ * @return bool The new $sentry_off setting.
+ */
+function jpry_fo_sentry_off( $sentry_off ) {
+
+	// Only process if the senty is NOT yet off
+	if ( ! $sentry_off ) {
+	
+		// Check for our custom Header
+		if ( 1 == getenv( 'X-WPE-FO-Cookie' ) ) {
+			$sentry_off = true;
+		}
+	}
+	
+	return $sentry_off;
+}
